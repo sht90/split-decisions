a7f6a2a6ecc8573a989d662eef41074f63e0d1e5
Sam Taylor <70077131+sht90@users.noreply.github.com>
2023-03-11 10:16:13 -0500

    bugfix decoding

--- SplitDecPuzzleCs/SplitDecisions/SplitDecisions/LetterCode.cs
+++ SplitDecPuzzleCs/SplitDecisions/SplitDecisions/LetterCode.cs
@@ -9,7 +9,7 @@ namespace SplitDecisions
     internal static class LetterCode
     {
         private static readonly Dictionary<char, int> Encoder = Enumerable.Range(0, 26).ToDictionary(x => (char)((int)'a' + x), x => 1 << x);
-        private static readonly Dictionary<int, char> Decoder = Enumerable.Range(0, 26).ToDictionary(x => 1 << x, x => (char)((int)'a' + x));
+        private static readonly Dictionary<int, char> Decoder = Enumerable.Range(0, 26).ToDictionary(x => x, x => (char)((int)'a' + x));
 
         public static int Encode(char letter)
         {
@@ -21,31 +21,12 @@ namespace SplitDecisions
             List<char> letters = new() { };
             for (int i = 0; i < 26; i++)
             {
-                if ((code | i) > 0)
+                if ((code & (1 << i)) > 0)
                 {
                     letters.Add(Decoder[i]);
                 }
             }
             return letters;
         }
-        /*
-        public static int Convert(char letter)
-        {
-            return 1 << ((int)letter - (int)'a');
-        }
-        public static List<char> Convert(int code)
-        {
-            int bitmask;
-            List<char> chars = new() { };
-            for (int i = 0; i < 26; i++)
-            {
-                bitmask = 1 << i;
-                if ((code | bitmask) == bitmask)
-                {
-                    chars.Add((char)((int)'a' + i));
-                }
-            }
-            return chars;
-        }*/
     }
 }
