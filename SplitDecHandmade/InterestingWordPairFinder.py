d690ea3d6c614228bac7aca41c4bfe5c2be79b3e
Sam Taylor <70077131+sht90@users.noreply.github.com>
2024-03-08 14:11:14 -0500

    finally get some interesting words

--- SplitDecHandmade/InterestingWordPairFinder.py
+++ SplitDecHandmade/InterestingWordPairFinder.py
@@ -7,16 +7,17 @@ Created on Tue Mar 5 15:49:32 2024
 # imports
 
 import functools
+import json
 import SplitDecisionsFinder as sdf
 from WordPair import WordPair
 
 # values
 
-IPA_DICTIONARY = 'ipa-dict-en_US.json'
+IPA_DICTIONARY = 'C:/Users/STaylor/Documents/TextFiles/ipa-dict-en_US.json'
 OUTPUT_PATH = 'C:/Users/STaylor/Documents/TextFiles'
 OUTPUT_FILENAME = 'interesting_word_pairs.txt'
 PATH = 'C:/Users/STaylor/Documents/TextFiles'
-FILENAME = 'google-10000-english-no-swears.txt'
+FILENAME = 'UsableDictionary.txt'
 MIN_WORD_LENGTH = 3
 MAX_WORD_LENGTH = 12
 
@@ -33,6 +34,9 @@ def score_interesting(word1, word2):
     # Deprioritize 3-letter words
     if (len(word1) == 3):
         score *= 0.9
+    # Deprioritize longer words
+    else:
+        score *= (1 + (7 - len(word1)) * 0.1)
     # Slightly deprioritize words that end in s
     if (word1[-1] == 's' or word2[-1] == 's'):
         score *= 0.95
@@ -40,6 +44,33 @@ def score_interesting(word1, word2):
     # Maybe this has some insight? https://linguistics.stackexchange.com/questions/30933/how-to-split-ipa-spelling-into-syllables
     # Prioritize words that have less-similar sounds
     # This repo will help you out a lot: https://github.com/mCodingLLC/Anaphones
+    try:
+        ipa1 = WORD_TO_IPA[word1]
+        ipa2 = WORD_TO_IPA[word2]
+    except KeyError:
+        return 0
+    except Exception as e:
+        print(f'Something wrong with dict\n{word1=}\n{word2=}\n{e=}')
+        return 1
+    try:
+        similarities = 0
+        emphasis_1 = ipa1.find('ˈ')
+        emphasis_2 = ipa2.find('ˈ')
+        emphasis_difference = abs(emphasis_1 - emphasis_2)
+        if (emphasis_1 < 0 or emphasis_2 < 0):
+            emphasis_difference = 0
+        if emphasis_difference > 2:
+            score *= 1.3
+        for c in ipa1:
+            if c in ipa2:
+                continue
+            similarities += 2
+        score *= (len(ipa1) + len(ipa2) / max(similarities, 2))
+    except Exception as e:
+        print(f'Something else wrong?\n{word1=}\n{word2=}\n{e=}')
+        return 1
+    #print('success!?!?!')
+
     # Lightly prioritize words that have an unusual amount of consecutive consonants or vowels
     most_consecutive_consonants = 0
     most_consecutive_vowels = 0
@@ -59,6 +90,8 @@ def score_interesting(word1, word2):
         score *= (0.8 + (most_consecutive_consonants * 0.1))
     if (most_consecutive_vowels >= 2):
         score *= (0.85 + (most_consecutive_vowels * 0.1))
+    if (most_consecutive_consonants == 0 or most_consecutive_vowels == 0):
+        return 0
     return score
 
 def cmp_word_pairs_interesting(word_pair_1, word_pair_2):
@@ -81,9 +114,21 @@ def main():
     print('Done parsing file!')
     word_pairs = sdf.find_word_pairs(words)
 
+    # Oh, we also need to parse the IPA dictionary
+    with open(IPA_DICTIONARY, 'r', encoding='utf-8') as fp:
+        (d,) = json.load(fp)['en_US']
+        for k, v in d.items():
+            kstrp = k.replace('"', '')
+            kstrp2 = kstrp.replace('"','')
+            sample_string = 'hi'
+            res = v if (',' not in v) else v.split(', ')[0]
+            break
+        global WORD_TO_IPA
+        WORD_TO_IPA = {k.replace('"', ''): (v if (',' not in v) else v.split(', ')[0] ) for k, v in d.items()}
+
     # Sort the word pairs by how cool they are, then print out the coolest ones
     word_pairs.sort(key=functools.cmp_to_key(cmp_word_pairs_interesting))
-    for wp in word_pairs[:20]:
+    for wp in word_pairs[:100]:
         print(wp)
 
 if __name__ == '__main__':
