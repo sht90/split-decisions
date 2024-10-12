1c3945ef515d7769fc479e3b6109c2c31403bef9
Sam Taylor <70077131+sht90@users.noreply.github.com>
2024-10-12 13:04:26 -0400

    create main file

---
+++ SplitDecisionsPython/SplitDecisionsPuzzleGenerator.py
@@ -0,0 +1,22 @@
+"""
+Sam Taylor
+October 10 2024
+
+Generate a Split Decisions puzzle
+
+For more details about Split Decisions puzzles, see Fred Piscop's
+website: https://split-decisions.us/
+"""
+
+def main():
+    pass
+    # Find word pairs from a list of words
+
+    # Find constraints for each word pair
+
+    # Create a board using word pairs and their constraints
+
+    # Save out the board and/or display it
+
+if __name__ == '__main__':
+    main()
