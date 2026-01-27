21627acf013f6002b74f23a4b9b8782c83043ee3
Sam Taylor <70077131+sht90@users.noreply.github.com>
2026-01-26 19:02:53 -0500

    draw empty cell svg

--- SplitDecBuilder/SplitDecisionsCellDrawerUtility.py
+++ SplitDecBuilder/SplitDecisionsCellDrawerUtility.py
@@ -9,6 +9,17 @@ Decisions board, which I hope to make in Godot.
 
 import drawsvg as draw
 
+def draw_empty_cell():
+    """draw a totally empty cell"""
+    # I feel like size should be arbitrary, it's more the aspect ratio
+    # that I care about? And that's always square, so... 100x100?
+    width = 100
+    height = 100
+    canvas = draw.Drawing(width, height, origin='top-left')
+    background = draw.Rectangle(0, 0, width, height, fill='#ffffff')
+    canvas.append(background)
+    return canvas
+
 def draw_corner_cell():
     """draw the sharp corner of a cell"""
     pass
@@ -31,7 +42,8 @@ def draw_two_steep_curves():
 
 def main():
     """draw all cells and save them as svgs"""
-    pass
+    empty_cell_canvas = draw_empty_cell()
+    empty_cell_canvas.save_svg('empty_cell.svg')
 
 if __name__ == '__main__':
     main()
