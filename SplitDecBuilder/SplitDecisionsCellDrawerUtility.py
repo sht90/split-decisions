f9f1258d8fc997d77b4455465a385fe6831a0868
Sam Taylor <70077131+sht90@users.noreply.github.com>
2026-01-26 19:23:54 -0500

    parameterize and draw corner

--- SplitDecBuilder/SplitDecisionsCellDrawerUtility.py
+++ SplitDecBuilder/SplitDecisionsCellDrawerUtility.py
@@ -9,20 +9,20 @@ Decisions board, which I hope to make in Godot.
 
 import drawsvg as draw
 
-def draw_empty_cell():
+def draw_empty_cell(width, height, background_color):
     """draw a totally empty cell"""
     # I feel like size should be arbitrary, it's more the aspect ratio
     # that I care about? And that's always square, so... 100x100?
-    width = 100
-    height = 100
     canvas = draw.Drawing(width, height, origin='top-left')
-    background = draw.Rectangle(0, 0, width, height, fill='#ffffff')
+    background = draw.Rectangle(0, 0, width, height, fill=background_color)
     canvas.append(background)
     return canvas
 
-def draw_corner_cell():
+def draw_corner_cell(width, height, background_color, foreground_color, stroke_thickness):
     """draw the sharp corner of a cell"""
-    pass
+    canvas = draw_empty_cell(width, height, background_color)
+    canvas.append(draw.Lines(height, 0, height, width, 0, width, close=False, fill='none', stroke=foreground_color, stroke_width=stroke_thickness))
+    return canvas
 
 def draw_edge_cell():
     """draw the flat edge of a cell"""
@@ -42,8 +42,15 @@ def draw_two_steep_curves():
 
 def main():
     """draw all cells and save them as svgs"""
-    empty_cell_canvas = draw_empty_cell()
+    width = 100
+    height = 100
+    background_color = '#ffffff'
+    foreground_color = '#000000'
+    stroke_thickness = 20
+    empty_cell_canvas = draw_empty_cell(width, height, background_color)
+    corner_cell_canvas = draw_corner_cell(width, height, background_color, foreground_color, stroke_thickness)
     empty_cell_canvas.save_svg('empty_cell.svg')
+    corner_cell_canvas.save_svg('corner_cell.svg')
 
 if __name__ == '__main__':
     main()
